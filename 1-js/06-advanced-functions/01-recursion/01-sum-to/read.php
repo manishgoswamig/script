c7a2fd20d2e20a30495818cd00b8d40dbd6d8809
Manish <manish.goswamij@gmail.com>
2019-04-21 19:32:37 +0530

    Added core package

---
+++ 1-js/06-advanced-functions/01-recursion/01-sum-to/read.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Functions</title>
+</head>
+<body>
+
+
+
+
+    <script type="text/javascript">
+
+        alert('why I am here !!!');
+        console.log('testing me...');
+
+
+    </script>
+</body>
+</html>
